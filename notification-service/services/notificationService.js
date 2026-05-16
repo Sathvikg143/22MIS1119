9798ffc016098f6554f9bb03ef1789fef77ac34a
Sathvikg143 <sathvikguptha81@gmail.com>
2026-05-16 17:32:57 +0530

    Added screenshots to correct folder and updated code

--- notification-service/services/notificationService.js
+++ notification-service/services/notificationService.js
@@ -1,42 +1,33 @@
 const axios = require("axios");
 const calculatePriority = require("../utils/priorityCalculator");
+
 const fetchNotifications = async (token) => {
   try {
-    const response = {
-  data: {
-    notifications: [
+    const response = await axios.get(
+      "http://4.224.186.213/evaluation-service/notifications",
       {
-        ID: "1",
-        Type: "Placement",
-        Message: "Google hiring drive",
-        Timestamp: "2026-04-22 17:51:18",
-      },
-      {
-        ID: "2",
-        Type: "Event",
-        Message: "Hackathon tomorrow",
-        Timestamp: "2026-04-22 18:00:00",
-      },
-      {
-        ID: "3",
-        Type: "Result",
-        Message: "Mid sem results published",
-        Timestamp: "2026-04-22 19:00:00",
-      },
-    ],
-  },
-};
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
     const notifications = response.data.notifications || [];
+
     const updatedNotifications = notifications.map((notification) => ({
       ...notification,
       priorityScore: calculatePriority(notification),
     }));
+
     updatedNotifications.sort(
       (a, b) => b.priorityScore - a.priorityScore
     );
-    return updatedNotifications;
+
+    return updatedNotifications.slice(0, 10);
   } catch (error) {
+    console.log(error.response?.data || error.message);
     throw new Error("Failed to fetch notifications");
   }
 };
+
 module.exports = fetchNotifications;
